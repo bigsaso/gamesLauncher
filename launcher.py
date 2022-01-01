c5f4dfa6ebe4c4aa08fc94d39edfbb5e161b09e9
bigsaso <bigsaso@DESKTOP-APDVLMI.localdomain>
2021-12-31 20:36:36 -0500

    Created first trial of choice

--- launcher.py
+++ launcher.py
@@ -1,4 +1,16 @@
 # This will be the app used to contain all the Games and their commands to run
 import subprocess
 
-subprocess.call(['sh', './MW.sh'])
+def MW():
+    subprocess.call(['sh','./MW.sh'])
+
+def hello():
+    print('Hello, World!')
+    exit()
+
+game = input("Choose a game: ")
+
+if game == 'MW':
+    MW()
+else:
+    hello()
